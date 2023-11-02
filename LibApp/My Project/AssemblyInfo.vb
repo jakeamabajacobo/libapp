98af917aaf93eb2c6811592122ef788e967a08c7
Jake Jacobo <jakejacobo2015@gmail.com>
2023-11-02 10:06:32 +0800

    added initial file

---
+++ LibApp/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("LibApp")>
+<Assembly: AssemblyDescription("ICT HANDSON EXAM PRACTICE")>
+<Assembly: AssemblyCompany("JJ PRODUCTION")>
+<Assembly: AssemblyProduct("LibApp")>
+<Assembly: AssemblyCopyright("Copyright ©  2023")>
+<Assembly: AssemblyTrademark("go fuck your self")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("7f70193d-1b79-42dc-a545-9f3bf5ff6b52")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
